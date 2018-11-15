315bd467242e6cbde397365a25d1e0f0a9747e2d
Ian Pösse <poesseian@googlemail.com>
2018-11-15 17:51:54 +0100

    added first working api entry

--- mitglied_web/test_ldap.php
+++ mitglied_web/test_ldap.php
@@ -7,7 +7,15 @@
 	ldap_set_option($ldapconn,LDAP_OPT_PROTOCOL_VERSION,3);
 	if ($ldapconn) {
 		ldap_start_tls($ldapconn);
-		$ldapbind = ldap_bind($ldapconn, "uid=normaltest,ou=user,dc=ldap-provider,dc=fablab-luebeck", "1234");
+		$sr = ldap_search($ldapconn, "ou=maschine,dc=ldap-provider,dc=fablab-luebeck", "(objectClass=*)", array("geraetname"));
+		$result = ldap_get_entries($ldapconn, $sr);
+		var_dump($result);
+		var_dump($result['count']);
+
+		echo ldap_error($ldapconn);
+		ldap_get_option($ldapconn, LDAP_OPT_DIAGNOSTIC_MESSAGE, $err);
+		echo "ldap_get_option: $err";
+		/*$ldapbind = ldap_bind($ldapconn, "uid=normaltest,ou=user,dc=ldap-provider,dc=fablab-luebeck", "1234");
 		
 		if ($ldapbind) {
 			echo "cool!";
@@ -16,6 +24,6 @@
 			echo ldap_error($ldapconn);
 			ldap_get_option($ldapconn, LDAP_OPT_DIAGNOSTIC_MESSAGE, $err);
 			echo "ldap_get_option: $err";
-		}
+		}*/
 	}
 ?>
