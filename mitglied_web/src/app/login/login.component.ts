3573400dc5fd94b1ea00ac00c7c702fde317ae9b
Ian Pösse <poesseian@googlemail.com>
2018-12-08 14:55:59 +0100

    refactored login component for #15

---
+++ mitglied_web/src/app/login/login.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  userQueryChanged: Subject<string> = new Subject<string>();
+  url_base:string = 'http://127.0.0.1/mitglied_web/';
+  validating : boolean = false;
+  valid : boolean = false;
+
+  @Input()
+  formGroup : FormGroup;
+  @Input()
+  passwordControl : string;
+  @Input()
+  usernameControl : string;
+
+  @Output()
+  validLogin = new EventEmitter<boolean>();
+
+  constructor(public http:HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.userQueryChanged
+        .pipe(debounceTime(500))
+        .subscribe(model => {
+          console.log(this.formGroup.value);
+          var user = this.sanitize(this.formGroup.value[this.usernameControl]);
+          var passw = this.sanitize(this.formGroup.value[this.passwordControl]);
+          var headers = new HttpHeaders();
+          var params = new HttpParams();
+          params = params.append('author_user', user);
+          params = params.append('author_password', passw);
+
+          this.http.get(this.url_base+'api/v1.0/index.php/Authentifizierung', {
+            headers: headers,
+            params: params
+          }).subscribe(data =>{
+              console.log("Authentifizierung erfolgreich: "+data);
+              this.validating = false;
+              this.valid = true;
+              this.validLogin.emit(true);
+            },
+            error => {
+              console.log("fetched error: ", error);
+              this.validating = false;
+              this.validLogin.emit(false);
+            });
+          });
+  }
+
+  changed() {
+    this.validating = true;
+    this.valid= false;
+    this.validLogin.emit(false);
+    this.userQueryChanged.next('');
+  }
+
+  sanitize(arg:string):string {
+    if (arg == undefined || arg == null) {
+      return "";
+    }
+    return arg;
+  }
+
+}
