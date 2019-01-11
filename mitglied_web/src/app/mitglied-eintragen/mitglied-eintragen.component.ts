e6e2f438483a87535a28f2a4723fcce179cb99cd
Ian Pösse <poesseian@googlemail.com>
2019-01-11 14:39:50 +0100

    WIP

--- mitglied_web/src/app/mitglied-eintragen/mitglied-eintragen.component.ts
+++ mitglied_web/src/app/mitglied-eintragen/mitglied-eintragen.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-mitglied-eintragen',
@@ -8,10 +12,43 @@ import { AppComponent } from '../app.component';
 })
 export class MitgliedEintragenComponent implements OnInit {
 
-  constructor(private appComponent:AppComponent) { }
+  constructor(private appComponent:AppComponent, private http:HttpClient, private formBuilder:FormBuilder) { }
+
+  loginForm: FormGroup;
 
   ngOnInit() {
-    this.appComponent.title = "Neues Mitglied eintragen"
+    this.appComponent.title = "Neues Mitglied eintragen";
+
+    this.loginForm = this.formBuilder.group({
+       username: [''],
+       password: [''],
+       //
+       neuesMitglied: [''],
+       //
+       mitgliedschaft: ['ehrenmitgliedschaft'],
+       anrede: [''],
+       titel: [''],
+       vorname: [''],//TODO:prefill
+       nachname: [''],//TODO:prefill
+       geburtsdatum: [''],//TODO:prefill
+       //
+       plz: [''],
+       ort: [''],
+       straße: [''],
+       //
+       email: [''],
+       telefon: [''],
+       notfallkontakt: [''],
+       //Nur wenn keine geteilte mitgliedschaft
+       iban: [''],
+       bic: [''],
+       kontoinhaber: [''],
+       //Nur wenn keine geteilte mitgliedschaft
+       beitragsreduzierung: [''],
+       ermaessigtBis: [''],
+       //Nur bei geteilter Mitgliedschaft
+       teilVon: ['']
+    });
   }
 
 }
