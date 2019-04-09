f325b373f75c9e7632c5120f1df23d70c37b08ae
Ian Pösse <poesseian@googlemail.com>
2019-04-09 15:37:41 +0200

    added basic startpage

--- mitglied_web/src/app/start/start.component.ts
+++ mitglied_web/src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomNavComponent } from '../custom-nav/custom-nav.component';
 
 @Component({
   selector: 'start',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(public customNavComponent:CustomNavComponent) {
+    
+  }
 
   ngOnInit() {
   }
