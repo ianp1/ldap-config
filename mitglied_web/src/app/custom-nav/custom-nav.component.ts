a56da8c1f3db2ef367d4d042a99e8007ad1ddfcf
Ian Pösse <poesseian@googlemail.com>
2019-04-09 14:23:49 +0200

    fixed #54

--- mitglied_web/src/app/custom-nav/custom-nav.component.ts
+++ mitglied_web/src/app/custom-nav/custom-nav.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { AppComponent } from '../app.component'
+import { AppComponent } from '../app.component';
+
+import { ViewChild } from '@angular/core';
+
+import { MatSidenav } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'custom-nav',
@@ -13,12 +18,31 @@ export class CustomNavComponent {
   valid = false;
   entry = "sicherheitsbelehrung";
 
+  isHandsetLocal:boolean = false;
+
+  @ViewChild('drawer')
+  sidenav : MatSidenav;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, public appComponent: AppComponent) {}
+
+  constructor(private breakpointObserver: BreakpointObserver,
+              public appComponent: AppComponent) {
+    this.isHandset$.subscribe(model=> {
+      this.isHandsetLocal = model;
+    });
+  }
+
+
+    selectEntry(entry: string) {
+      this.entry='sicherheitsbelehrung';
+
+      if (this.isHandsetLocal) {
+        this.sidenav.close();
+      }
+    }
 
 }
