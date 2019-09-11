14c0b3c2d1edc48fc35168b4933b3feab5b3c689
Ian Pösse <ian@ipoesse.de>
2019-09-12 00:49:50 +0200

    add working websocket implementation

--- masterterminal/rfid_reader.py
+++ masterterminal/rfid_reader.py
@@ -2,13 +2,28 @@ import asyncio
 import websockets
 import json
 
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
 async def rfid(websocket, path):
+    try:
+        while True:
+            id, text = reader.read()
+            tag = hex(id)[2:-2]
+            message = {"rfid":tag}
+            await websocket.send(json.dumps(message))
+    finally:
+        GPIO.cleanup()
+    '''
     print("received websocket connection")
     message = {"rfid":"2237FAE4"}
     await websocket.send(json.dumps(message))
     print("returned rfid")
+    '''
 
-start_server = websockets.serve(rfid, "localhost", 8765)
+start_server = websockets.serve(rfid, "192.168.2.55", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
