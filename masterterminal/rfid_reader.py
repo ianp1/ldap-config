39a1adde413efe87aa19b08eb8e0c89750727a89
Ian Pösse <ian@ipoesse.de>
2019-09-14 20:00:06 +0200

    add autobahn library code

--- masterterminal/rfid_reader.py
+++ masterterminal/rfid_reader.py
@@ -8,14 +8,14 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 async def rfid(websocket, path):
+    print("called rfid")
     try:
-        while True:
-            id, text = reader.read()
-            tag = hex(id)[2:-2]
-            message = {"rfid":tag}
-            await websocket.send(json.dumps(message))
-    finally:
-        GPIO.cleanup()
+        id, text = reader.read()
+        tag = hex(id)[2:-2]
+        message = {"rfid":tag}
+        await websocket.send(json.dumps(message))
+    except Error:
+        print("error")
     '''
     print("received websocket connection")
     message = {"rfid":"2237FAE4"}
@@ -25,5 +25,8 @@ async def rfid(websocket, path):
 
 start_server = websockets.serve(rfid, "192.168.2.55", 8765)
 
+#print("loop")
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
+GPIO.cleanup()
